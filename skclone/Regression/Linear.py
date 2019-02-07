7b6e519fb795ff6b902cec0543438550418e3772
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-02-07 13:50:33 +0530

    Regression .Predict works with tuples/list instead of singular values

--- skclone/Regression/Linear.py
+++ skclone/Regression/Linear.py
@@ -8,20 +8,23 @@ class Linear():
         self.bias = 0
 
     def fit(self, X, y):
-        assert len(X) == len(y), "X and y shapes are not same , x:shape={} and y:shape={}".format(len(X), len(y))
+        assert len(X) == len(
+            y), "X and y shapes are not same , x:shape={} and y:shape={}".format(len(X), len(y))
         self.X = X
         self.y = y
         x_bar = sum(X)/len(X)
         y_bar = sum(y)/len(y)
-        m1 = [ _ - x_bar for _ in self.X]
-        m2 = [ _ - y_bar for _ in self.y]
-        d1 = [( _ - x_bar)**2 for _ in self.X]
+        m1 = [_ - x_bar for _ in self.X]
+        m2 = [_ - y_bar for _ in self.y]
+        d1 = [(_ - x_bar)**2 for _ in self.X]
         numer = []
         for _ in range(len(X)):
-        	numer.append(m1[_] * m2[_])
+            numer.append(m1[_] * m2[_])
 
         self.coef = sum(numer)/sum(d1)
         self.bias = y_bar - self.coef*(x_bar)
 
     def predict(self, data):
-        return self.coef*data + self.bias
+        """Accepts a list or a tuple of data and sends the corresponding y values"""
+        def pred(x): return self.coef*x + self.bias
+        return map(pred, data)
