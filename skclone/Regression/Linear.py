378c73b7c1c764bfa226987de0a1c3b198b8618f
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-02-07 13:38:33 +0530

    Linear Regression works

--- skclone/Regression/Linear.py
+++ skclone/Regression/Linear.py
@@ -8,21 +8,20 @@ class Linear():
         self.bias = 0
 
     def fit(self, X, y):
-        assert len(X) == len(
-            y), "X and y shapes are not same , x:shape={} and y:shape={}".format(len(X), len(y))
+        assert len(X) == len(y), "X and y shapes are not same , x:shape={} and y:shape={}".format(len(X), len(y))
         self.X = X
         self.y = y
-        self.x_bar = sum(X)/len(X)
-        self.y_bar = sum(y)/len(y)
-        m1 = [_ - self.x_bar for _ in self.X]
-        m2 = [_ - self.y_bar for _ in self.y]
-        d1 = [(_ - self.x_bar)**2 for _ in self.X]
+        x_bar = sum(X)/len(X)
+        y_bar = sum(y)/len(y)
+        m1 = [ _ - x_bar for _ in self.X]
+        m2 = [ _ - y_bar for _ in self.y]
+        d1 = [( _ - x_bar)**2 for _ in self.X]
         numer = []
         for _ in range(len(X)):
-        	numer[_] = m1[_] * m2[_]
+        	numer.append(m1[_] * m2[_])
 
         self.coef = sum(numer)/sum(d1)
-        self.bias = self.y_bar - self.coef*(self.x_bar)
+        self.bias = y_bar - self.coef*(x_bar)
 
     def predict(self, data):
-        return = self.coef*data + self.bias
+        return self.coef*data + self.bias
