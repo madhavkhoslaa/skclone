21d0908fd01671685c060cd11728286b524f599d
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-28 17:16:31 +0530

    KNN

--- skclone/Clustering/KNN.py
+++ skclone/Clustering/KNN.py
@@ -1,6 +1,12 @@
 class KNN():
     def __init__(self, neighbours):
-        self.neighbours= neighbours
+        self.neighbours = neighbours
+
     def fit(self, X, y):
-        self.X= X
-        self.y= y        
+        assert len(X) == len(
+            y), "X and y shapes are not same , x:shape={} and y:shape={}".format(len(X), len(y))
+        self.X = X
+        self.y = y
+
+    def predict(self, x_):
+        pass
