758eef88649048001ef203e03a481d659f64f714
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-28 17:16:52 +0530

    Regression pep

--- skclone/Regression/Linear.py
+++ skclone/Regression/Linear.py
@@ -1,7 +1,7 @@
 
 class Linear():
-    """Performs the least squares regression aka linear 
-	regression on the data "fitted" onto its instance"""
+    """Performs the least squares regression aka linear
+        regression on the data "fitted" onto its instance"""
 
     def __init__(self):
         self.coef = 0
@@ -12,8 +12,8 @@ class Linear():
             y), "X and y shapes are not same , x:shape={} and y:shape={}".format(len(X), len(y))
         self.X = X
         self.y = y
-        x_bar = sum(X)/len(X)
-        y_bar = sum(y)/len(y)
+        x_bar = sum(X) / len(X)
+        y_bar = sum(y) / len(y)
         m1 = [_ - x_bar for _ in self.X]
         m2 = [_ - y_bar for _ in self.y]
         d1 = [(_ - x_bar)**2 for _ in self.X]
@@ -21,10 +21,10 @@ class Linear():
         for _ in range(len(X)):
             numer.append(m1[_] * m2[_])
 
-        self.coef = sum(numer)/sum(d1)
-        self.bias = y_bar - self.coef*(x_bar)
+        self.coef = sum(numer) / sum(d1)
+        self.bias = y_bar - self.coef * (x_bar)
 
     def predict(self, data):
         """Accepts a list or a tuple of data and sends the corresponding y values"""
-        def pred(x): return self.coef*x + self.bias
+        def pred(x): return self.coef * x + self.bias
         return map(pred, data)
